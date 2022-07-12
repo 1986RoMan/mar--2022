295560771c400c6e8965de947a49c85927e70b1b
partyzan1986 <partyzan1986@gmail.com>
2022-07-12 13:40:18 +0300

    zmina

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+
 import {Comments, Posts} from "./components";
 
 function App() {
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+
 import {Post} from "../Post/Post";
 
 class Posts extends Component {
