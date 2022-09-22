fd5e8a3c04ff948e5e8f7c906139647ddca9dac1
partyzan1986 <partyzan1986@gmail.com>
2022-09-22 19:02:23 +0300

    zmina

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Post = ({post}) => {
     return (
-        <div style={{border: '1px solid black',margin:'2px'}}>
+        <div
+            style={{border: '1px solid black',margin:'2px'}}>
             {post.title}
         </div>
     );
